61899a07b60e5b0642e6d825c16fd3d2c3925d74
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-03-11 11:25:33 -0300

    instala react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Characters from './components/Characters';
-import CharacterDetails from './components/CharactersDetails';
-import Episode from './components/Episode';
-import EpisodeDetails from './components/EpisodeDetails';
-import About from './components/About';
-import NotFound from './components/NotFound';
+// import Login from './components/Login';
+// import Characters from './components/Characters';
+// import CharacterDetails from './components/CharactersDetails';
+// import Episode from './components/Episode';
+// import EpisodeDetails from './components/EpisodeDetails';
+// import About from './components/About';
+// import NotFound from './components/NotFound';
 
 class App extends Component {
   render() {
@@ -15,13 +15,13 @@ class App extends Component {
         <p>Monitoria do Douglas</p>
         <BrowserRouter>
             <Switch>
-              <Route exact path="/" component={ Login } />
+              {/* <Route exact path="/" component={ Login } />
               <Route path="/characters" component={ Characters } />
               <Route path="/characters/:id" component={ CharacterDetails } />
               <Route path="/episodes" component={ Episode } />
               <Route path="/episodes/:id" component={ EpisodeDetails } />
               <Route path="/about" component={ About } />
-              <Route path="*" component={ NotFound } />
+              <Route path="*" component={ NotFound } /> */}
             </Switch>
           </BrowserRouter>
 
