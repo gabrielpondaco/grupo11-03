5a5be3520fffe841880e8b9bd3b0f1844e1644ae
jose <carlossouzalimajose@gmail.com>
2022-03-11 11:28:15 -0300

    altera o header

--- src/header.jsx
+++ src/header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from 'react-dom-route'
+import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
   render() {
     return (
       <>
-        <h1>teste</h1>
+        <h1>Rick and Morty</h1>
         <Link to="/characters">
           <p>characters</p>
         </Link>
